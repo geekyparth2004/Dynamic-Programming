ba3dda3f9861864b5dbcfd0a4654e9bea0e90db7
geekyparth2004 <goelparth20049@gmail.com>
2025-02-15 12:37:10 +0530

    Update

---
+++ Day-1/05.746.min-cost-climbing-stairs.cpp
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode id=746 lang=cpp
+ *
+ * [746] Min Cost Climbing Stairs
+ *
+ * https://leetcode.com/problems/min-cost-climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (66.42%)
+ * Likes:    11757
+ * Dislikes: 1814
+ * Total Accepted:    1.4M
+ * Total Submissions: 2.1M
+ * Testcase Example:  '[10,15,20]'
+ *
+ * You are given an integer array cost where cost[i] is the cost of i^th step
+ * on a staircase. Once you pay the cost, you can either climb one or two
+ * steps.
+ * 
+ * You can either start from the step with index 0, or the step with index 1.
+ * 
+ * Return the minimum cost to reach the top of the floor.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: cost = [10,15,20]
+ * Output: 15
+ * Explanation: You will start at index 1.
+ * - Pay 15 and climb two steps to reach the top.
+ * The total cost is 15.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: cost = [1,100,1,1,1,100,1,1,100,1]
+ * Output: 6
+ * Explanation: You will start at index 0.
+ * - Pay 1 and climb two steps to reach index 2.
+ * - Pay 1 and climb two steps to reach index 4.
+ * - Pay 1 and climb two steps to reach index 6.
+ * - Pay 1 and climb one step to reach index 7.
+ * - Pay 1 and climb two steps to reach index 9.
+ * - Pay 1 and climb one step to reach the top.
+ * The total cost is 6.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 2 <= cost.length <= 1000
+ * 0 <= cost[i] <= 999
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int helper(vector<int>& cost , int i , vector<int> &dp)
+    {   int n = cost.size();
+        if(i == 0 or i == 1) return cost[i]; 
+        if(dp[i]!=-1) return dp[i];
+        return dp[i] = cost[i] + min(helper(cost,i-1,dp) , helper(cost,i-2,dp));
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        vector<int> dp(1005,-1); 
+        int n = cost.size(); 
+        return min(helper(cost,n-1,dp) , helper(cost,n-2,dp)); 
+    }
+};
+// @lc code=end
+
